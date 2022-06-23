234568ef4adcb974418aebfc6de8f0d85de6bab1
sudosilico <nicksilico@gmail.com>
2022-06-23 02:14:18 -0400

    Draggables progress

--- src/draggable/DraggableStage.stories.tsx
+++ src/draggable/DraggableStage.stories.tsx
@@ -1,4 +1,8 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Konva from "konva";
+import { useCallback, useRef } from "react";
+import { Group, Layer, Rect, Stage } from "react-konva";
+import { DraggableSquare } from "./DraggableSquare";
 import { DraggableStage, DraggableStageProps } from "./DraggableStage";
 
 export default {
@@ -7,12 +11,13 @@ export default {
   argTypes: {},
 } as ComponentMeta<typeof DraggableStage>;
 
-const Template: ComponentStory<typeof DraggableStage> = (args: DraggableStageProps) => {
-  return <DraggableStage {...args} />;
+const defaultProps: DraggableStageProps = {
+  width: 500,
+  height: 500,
 };
 
-const defaultProps: DraggableStageProps = {
-  // TODO: Add default props
+const Template: ComponentStory<typeof DraggableStage> = (args: DraggableStageProps) => {
+  return <DraggableStage {...args} />;
 };
 
 const create = (args?: Partial<DraggableStageProps>) => {
@@ -21,4 +26,67 @@ const create = (args?: Partial<DraggableStageProps>) => {
   return instance;
 };
 
-export const Basic = create();
+export const Basic = () => {
+  return (
+    <DraggableStage width={500} height={500}>
+      <Layer>
+        <DraggableSquare color="black" />
+        <DraggableSquare color="red" />
+        <DraggableSquare color="yellow" />
+      </Layer>
+    </DraggableStage>
+  );
+};
+
+const DragGroupTest = () => {
+  const dragRef = useRef<Konva.Rect>(null);
+  const groupRef = useRef<Konva.Group>(null);
+
+  const onDragStart = useCallback(() => {
+    const drag = dragRef.current;
+    const group = groupRef.current;
+    const stage = group?.getStage();
+
+    if (!drag || !group || !stage) {
+      return;
+    }
+
+    drag.stopDrag();
+
+    const size = group.getClientRect();
+    const pos = stage.getPointerPosition();
+
+    group.startDrag();
+  }, []);
+
+  return (
+    <>
+      <Group ref={groupRef}>
+        <Rect
+          ref={dragRef}
+          x={100}
+          y={0}
+          width={50}
+          height={50}
+          fill={"blue"}
+          draggable
+          onDragStart={onDragStart}
+        />
+        <Rect x={100} y={50} width={50} height={50} fill={"cyan"} />
+      </Group>
+    </>
+  );
+};
+
+export const Test1 = () => {
+  return (
+    <Stage width={650} height={650}>
+      <Layer>
+        <Rect width={650} height={650} fill={"gray"} />
+      </Layer>
+      <Layer>
+        <DragGroupTest />
+      </Layer>
+    </Stage>
+  );
+};
